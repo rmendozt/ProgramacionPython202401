7a12ece9681779479cf64408b28bec275e36ba9f
gdelgador <67815621+gdelgador@users.noreply.github.com>
2024-01-21 18:01:29 +0000

    carga modulo1

---
+++ Modulo1/scripts/area.py
@@ -0,0 +1,5 @@
+base = int(input("Ingrese la base: "))
+altura = int(input("Ingrese la altura: "))
+
+area = base * altura/2
+print(area)
---
+++ Modulo1/scripts/listas.py
@@ -0,0 +1,15 @@
+# crear 10 variables
+
+# listas -> colecciones de datos 
+#          las listas me ayudan a almacenar un conjunto de valores
+
+lista_valores = ["gola", "xyz", 123, True, 34.5, False]
+
+
+print(f'Imprimo elemento 0 de la lista {lista_valores[0]}')
+
+
+
+print(f'Imprimo elemento ultimo elemento de la lista {lista_valores[-1]}')
+
+
